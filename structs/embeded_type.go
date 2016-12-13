23c461e00ee82887e428f6fb76baf9409b9d2577
Long Tran <long.tran@vsource.io>
2016-12-13 09:27:32 +0700

    Struct WIP

---
+++ structs/embeded_type.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+type Person struct {
+	Name string
+}
+
+type Android struct {
+	Person
+	Model string
+}
+
+func (p *Person) Talk() {
+	fmt.Println("Hi, my name is", p.Name)
+}
+
+func main() {
+	p := Person{"Long"}
+	a := new(Android)
+	a.Talk()
+	p.Talk()
+}
---
+++ structs/second_struct.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	x, y, r float64
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+
+	return math.Sqrt(a*a + b*b)
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x2, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+
+	return l * w
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	c := Circle{0, 0, 5}
+	r := Rectangle{0, 0, 10, 10}
+
+	fmt.Printf("Circle area: %f\n", c.area())
+	fmt.Printf("Rectangle area: %f\n", r.area())
+}
